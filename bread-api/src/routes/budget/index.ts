1e5af2f829564754626f6790ecd5e0ace748fd5e
Srinand Diddi <srinand.com@gmail.com>
2026-02-16 21:53:09 +0530

    api: refactor budget endpoint naming

--- bread-api/src/routes/budget/index.ts
+++ bread-api/src/routes/budget/index.ts
@@ -1,10 +1,15 @@
 import { FastifyInstance } from "fastify";
 import {getBudgetsHandler, getBudgetMonthHandler, getBudgetHandler} from "./handlers";
 
-async function budgetRoutes(fastify: FastifyInstance, options: any) {
-    fastify.get('/budgetMonth/:budgetId/:month', getBudgetMonthHandler)
+async function budgetRoutes(fastify: FastifyInstance) {
+    // get budget month
+    fastify.get('/budgets/:budgetId/months/:month', getBudgetMonthHandler)
+
+    // get single budget
+    fastify.get('/budgets/:budgetId', getBudgetHandler)
+
+    // list budgets
     fastify.get('/budgets', getBudgetsHandler)
-    fastify.get('/budget/:budgetId', getBudgetHandler)
 }
 
 export default budgetRoutes
