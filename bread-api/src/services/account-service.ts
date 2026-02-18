cf13eeef88f94fc2f4d0d6e8d16583862952b815
lollyxsrinand <srinand.com@gmail.com>
2026-02-18 21:27:47 +0530

    api: initialise account balance through ready to assign transaction

--- bread-api/src/services/account-service.ts
+++ bread-api/src/services/account-service.ts
@@ -1,18 +1,20 @@
 import { Account } from "bread-core/src"
 import { db } from "../firebase/server"
+import { createTransaction } from "./transaction-service"
 
-export const createAccount = async (uid: string, budgetId: string, data: {name: string, type: string, balance: number}) => {
-        const accountRef = db.collection('users').doc(uid).collection('budgets').doc(budgetId).collection('accounts').doc()
+export const createAccount = async (userId: string, budgetId: string, data: {name: string, type: string, balance: number}) => {
+        const accountRef = db.collection('users').doc(userId).collection('budgets').doc(budgetId).collection('accounts').doc()
     
         const accountData: Account = {
             id: accountRef.id,
             name: data.name,
             type: data.type,
-            balance: data.balance,
+            balance: 0,
             createdAt: Date.now(),
         }
     
         await accountRef.set(accountData)
+        await createTransaction(userId, budgetId, accountRef.id, null, 'readytoassign', data.balance, new Date())
     
         return accountData
 }
