730a265224a7ee447a38ff578eb0fd6afe9087ae
lollyxsrinand <srinand.com@gmail.com>
2026-03-10 23:17:00 +0530

    refactor(web): show text why login is disabled in prod

--- bread-web/app/page.tsx
+++ bread-web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className='h-screen w-full flex items-center justify-center'>
-      <div className="h-fit w-fit flex flex-col gap-5 justify-center items-center p-5">
+      <div className="h-fit w-64 flex flex-col gap-5 justify-center items-center p-5">
         <div className='flex w-full text-center items-center'>
           <h1 className="font-bold w-full">hi</h1>
           <h4 className="w-full">this is <a target="_blank" href="https://github.com/lollyxsrinand/bread" className='font-bold text-orange-200 underline underline-offset-8 hover:text-orange-300 transition-all'>bread</a></h4>
@@ -9,8 +9,9 @@ export default function Page() {
         <div>
           <p className="font-extralight">a budget management <a target='_blank' href="https://ynab.com/" className="text-blue-300 underline-offset-4 font-medium hover:text-blue-400 transition-all">clone</a></p>
         </div>
-         <div className="flex gap-2.5 w-full items-center justify-center text-center">
+         <div className="flex flex-col gap-2.5 w-full items-center justify-center text-center">
            <a className="button w-full hover:bg-white hover:text-black" href="/login">login</a>
+           {process.env.NODE_ENV === "production" && <p className="text-neutral-600">nah the button wont work we{`'`}re still making it</p>}
          </div>
       </div>
     </div>
